7921a972609220c5d90a46ae332a2a75e5f6e331
Duarte Almeida <duartemalmeida@gmail.com>
2022-03-12 12:10:46 +0000

    fix: change the request to post for passing body with it.
    feat: add from and size properties to body
    
    fix: catch error in console in try/catch block

--- pages/api/quotes.js
+++ pages/api/quotes.js
@@ -3,23 +3,24 @@ import axios from 'axios';
 const baseURL = 'http://cosmic-elastic.eu.ngrok.io/shakespeare/_search';
 
 const bodyData = {
+  from: 10,
+  size: 100,
   query: {
     match_phrase: {
-      message: '',
+      text_entry: '',
     },
   },
 };
-
 export let failedRequest = false;
 
 export const getData = async (searchStr) => {
-  bodyData.query.match_phrase.message = searchStr;
-  console.log(bodyData);
+  bodyData.query.match_phrase.text_entry = searchStr;
   try {
-    const res = await axios.get(baseURL, bodyData);
+    const res = await axios.post(baseURL, bodyData);
     const data = res.data;
     console.log(data);
-  } catch {
+    return data;
+  } catch (error) {
     console.log(error);
     failedRequest = true;
   }
