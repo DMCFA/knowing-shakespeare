c64532330c096d61220c7de2ec4f0abb9592813b
Duarte Almeida <duartemalmeida@gmail.com>
2022-03-13 10:50:01 +0000

    feat: remove image, keep text only

--- components/Nav.js
+++ components/Nav.js
@@ -4,12 +4,6 @@ const Nav = () => {
   return (
     <div className='nav-container'>
       <h1>knowing shakespeare</h1>
-      <Image
-        src='/shakespeare-nav.png'
-        alt='Shakespeare image'
-        width={100}
-        height={50}
-      ></Image>
     </div>
   );
 };
