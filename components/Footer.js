75a1d32d3de03926894adc9994e6e1157b775877
Duarte Almeida <duartemalmeida@gmail.com>
2022-03-13 11:03:35 +0000

    feat: add footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>knowing shakespeare © 2022 - inspired by the COSMIC team</p>
+    </div>
+  );
+};
+
+export default Footer;
